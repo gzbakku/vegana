3cee43112a3a4b3f785493203a8fde48982ffa67
gzbakku <gzbakku@gmail.com>
2019-06-11 18:32:53 +0530

    base app index was updated to comply with the new seo tracker system

--- bin/build/app/index.js
+++ bin/build/app/index.js
@@ -1,21 +1,18 @@
-//import page which you want to load in the main bundle/ at the beggning of the app
+
+//import all the pages here which you want to be in the app and use engine.get.pageModule api to get the page
 const mainPage = require('./pages/mainPage/page');
 
-//declare the firest page module here
+//declare the first page module here
 const startPage = mainPage;
+
+/*set the base url to the native vegana cdn,
+or if hosting on non native platform please
+set the baseurl to where the files for the project are held*/
 const baseHref = null;
 
 //------------------------------------------------------------------------------
 //dont fuck with anything below
-
 engine.router.set.baseHref(baseHref);
-
 if(engine.router.active.page == null){
-  loadPage();
-}
-
-function loadPage(){
-  engine.router.active.page = startPage.ref;
-  engine.router.built.page.push(startPage.ref);
   startPage.init();
 }
